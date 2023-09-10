eb703377caa02c0c054756543759187b85a463eb
emadkhan <emadkhan@lupindigitalhealth.com>
2023-09-10 13:53:55 +0530

    first appo

---
+++ app/src/main/java/com/example/serviceclient/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.serviceclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName("com.example.sosservice", "com.example.sosservice.SOSService"));
+        startForegroundService(intent);
+    }
+}
